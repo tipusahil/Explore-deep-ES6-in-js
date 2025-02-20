5b59c78f89e15286072b3218ad9c8fba090e4292
tipusahil <tipusahil.ctg@gmail.com>
2025-02-20 08:35:48 +0600

    array of object e map related method use

---
+++ array_of_object.js
@@ -0,0 +1,46 @@
+const laptops = [
+    {id:1 , name:'hp core i7' , price: 50000},
+    {id:2 , name:'lenovo' , price: 35000},
+    {id:3 , name:'macbook' , price: 140000},
+    {id:4 , name:'dell' , price: 40000},
+]
+;
+const  laptop_names = laptops.map(laptop => laptop.name );// [ 'hp core i7', 'lenovo', 'macbook', 'dell' ]
+console.log(laptop_names)
+
+// example of map() 2:
+const laptopNameAndPrice = laptops.map(laptop => `laptop name =${laptop.name} and price is =${laptop.price}`);
+console.log(laptopNameAndPrice);
+
+
+// example of map() 3:
+const laptop_price = laptops.map(laptop => laptop.price );//[ 50000, 35000, 140000, 40000 ]
+console.log(laptop_price)
+
+
+laptops.forEach(laptop1 => console.log(laptop1.name)); 
+/*output:
+ hp core i7
+lenovo
+macbook
+dell
+
+ekane forEach jeheto kisu return korena tai kuno varible declare kore then forEach calanor mane hoina karon oi variable e to kisu return korbnea. so outut pawa jabena tai direct arrayName.forEach() use kora hoi.kuno variable declare na kore.
+forEach() method array er prottekta element er map er moto kaj kore .loop calanu hoi jei oparetion ta korte bola hoi setaa korbe kintu kisu return dibena. hardcoded vabe (return) keyword liklew return dibena.just arry er upodan er upore jeta korte bola hobe seta kore ghumai porbe.
+*/
+
+
+// example filter 4:
+const expensive_laptop = laptops.filter(laptop => laptop.price >50000);// [ { id: 3, name: 'macbook', price: 140000 } ]
+console.log(expensive_laptop)
+
+// example filter 5:
+const komDamiLaptop= laptops.filter(lap => lap.price <41000);
+/* [
+  { id: 2, name: 'lenovo', price: 35000 },
+  { id: 4, name: 'dell', price: 40000 }
+] */
+console.log(komDamiLaptop)
+
+const komDamirModdeProtomta = laptops.find(lap1kore => lap1kore.price <41000);// { id: 2, name: 'lenovo', price: 35000 }
+console.log(komDamirModdeProtomta);
---
+++ reduce_method.js
@@ -0,0 +1,20 @@
+const numbers = [1,4,5,5,5];
+const newNum = numbers.reduce((pera1, pera2) => pera1 + pera2);
+//    newNum = 20
+console.log(newNum);
+
+/* 
+// reduce() method er ketre 2ta peramitar lage.
+array er prottekta element k er jei oparetion ta  korte bola hobe seta kore total result ta return kore , kuno notun array create korbena.karon prottekta element er total result ta dekabe.element dekabena.tai,just array er element gulor protom elment theke shuro kore ekdm sesh element obdi sobgulote oparetion calai total result  return kore. */
+
+// example 2:
+const numbers1 = [2,3,4,5];
+const total1 =numbers1.reduce((x , y) => x*y);
+//    total1 = 120
+console.log(total1)
+
+
+const friends = [20,20,20];
+// const totalResult = friends.reduce(( a , b) => a.length === b.length);// =false
+const totalResult = friends.reduce(( a , b) => a + b );// = 60
+console.log(totalResult)
